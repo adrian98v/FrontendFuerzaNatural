8c820e93331059c15ea58a79679e88bac2cc016a
Adrian Veppo <veppoadrian98@gmail.com>
2025-04-11 13:53:50 -0300

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,36 +1,64 @@
-import './login.css'
+import './login.css';
 import logo from '../assets/Logo_SinFondo_MásChico.png';
-
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [contrasenia, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    return (
-      <div className="login-container">
-        <form className="form">
-        <a className='a_logo' href="/"><img src={logo} alt="Logo" className="Logo" /></a>
+  const handleLogin = async (e) => {
+    e.preventDefault(); 
 
-          <h2 className="title">Iniciar Sesión</h2>
-  
-          
-          <input
-            type="email"
-            placeholder="Email"
-            className="input"
-          />
-  
-          <input
-            type="password"
-            placeholder="Contraseña"
-            className="input"
-          />
-  
-          <button type="submit" className="btn">
-            Ingresar
-          </button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default Login;
-  
+    try {
+      const response = await axios.post('http://localhost:3000/login', {
+        email,
+        contrasenia
+      });
+
+      console.log('Login exitoso:', response.data);
+      navigate('/'); // redirige al home
+
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form className="form" onSubmit={handleLogin}>
+        <a className="a_logo" href="/">
+          <img src={logo} alt="Logo" className="Logo" />
+        </a>
+
+        <h2 className="title">Iniciar Sesión</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Contraseña"
+          className="input"
+          value={contrasenia}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit" className="btn">
+          Ingresar
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
